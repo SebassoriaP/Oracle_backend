07029c85fe5517f3db50ba1963c71d81002c8f09
DaniloTato <Noentiendo2005@gmail.com>
2026-05-21 09:26:56 -0600

    Modify ingress.yaml

--- src/main/java/com/example/omi/health/HealthController.java
+++ src/main/java/com/example/omi/health/HealthController.java
@@ -20,6 +20,6 @@ public class HealthController {
   @GetMapping("/health")
   public Map<String, Object> health() {
     Integer result = jdbcTemplate.queryForObject("SELECT 1 FROM DUAL", Integer.class);
-    return Map.of("status", "ok_2", "db", result);
+    return Map.of("status", "ok_3", "db", result);
   }
 }
